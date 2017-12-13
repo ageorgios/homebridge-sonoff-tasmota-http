7eafce98e44e1c067e69a961d89c08acb12b222a
DanTheMan827 <790119+DanTheMan827@users.noreply.github.com>
2017-12-12 22:25:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function SonoffTasmotaHTTPAccessory(log, config) {
   this.log = log;
   this.config = config;
   this.name = config["name"]
-  this.relay = config["relay"] || "1"
+  this.relay = config["relay"] || ""
   this.hostname = config["hostname"] || "sonoff";
 
   this.service = new Service.Outlet(this.name);
@@ -32,8 +32,8 @@ SonoffTasmotaHTTPAccessory.prototype.getState = function(callback) {
     if (error) return callback(error);
   	var lines = body.split("\n");
   	that.log("Sonoff HTTP: " + that.hostname + ", Relay " + that.relay + ", Get State: " + lines[1]);
-  	if (lines[1] == "POWER = OFF") callback(null, 0)
-  	else if (lines[1] == "POWER = ON") callback(null, 1)
+  	if (lines[1] == "POWER" + that.relay + " = OFF") callback(null, 0)
+  	else if (lines[1] == "POWER" + that.relay + " = ON") callback(null, 1)
   })
 }
 
@@ -45,8 +45,8 @@ SonoffTasmotaHTTPAccessory.prototype.setState = function(toggle, callback) {
     if (error) return callback(error);
   	var lines = body.split("\n");
   	that.log("Sonoff HTTP: " + that.hostname + ", Relay " + that.relay + ", Set State to: " + lines[1]);
-  	if (lines[1] == "POWER = OFF") callback()
-  	else if (lines[1] == "POWER = ON") callback()
+  	if (lines[1] == "POWER" + that.relay + " = OFF") callback()
+  	else if (lines[1] == "POWER" + that.relay + " = ON") callback()
   })
 }
 
