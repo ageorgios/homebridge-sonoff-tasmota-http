4cc73de8b7e318ee874b69cd26fe0493df399b7b
DanTheMan827 <790119+DanTheMan827@users.noreply.github.com>
2017-12-12 21:51:30 -0600

    Add support for multiple relay sonoff devices

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function SonoffTasmotaHTTPAccessory(log, config) {
   this.log = log;
   this.config = config;
   this.name = config["name"]
+  this.relay = config["relay"] || "1"
   this.hostname = config["hostname"] || "sonoff";
 
   this.service = new Service.Outlet(this.name);
@@ -27,10 +28,10 @@ function SonoffTasmotaHTTPAccessory(log, config) {
 
 SonoffTasmotaHTTPAccessory.prototype.getState = function(callback) {
   var that = this
-  request("http://" + that.hostname + "/cm?cmnd=Power", function(error, response, body) {
+  request("http://" + that.hostname + "/cm?cmnd=Power" + that.relay, function(error, response, body) {
     if (error) return callback(error);
   	var lines = body.split("\n");
-  	that.log("Sonoff HTTP: " + that.hostname + " Get State: " + lines[1]);
+  	that.log("Sonoff HTTP: " + that.hostname + ", Relay " + that.relay + ", Get State: " + lines[1]);
   	if (lines[1] == "POWER = OFF") callback(null, 0)
   	else if (lines[1] == "POWER = ON") callback(null, 1)
   })
@@ -40,10 +41,10 @@ SonoffTasmotaHTTPAccessory.prototype.setState = function(toggle, callback) {
   var newstate = "%20Off"
   if (toggle) newstate = "%20On"
   var that = this
-  request("http://" + that.hostname + "/cm?cmnd=Power" + newstate, function(error, response, body) {
+  request("http://" + that.hostname + "/cm?cmnd=Power" + that.relay + newstate, function(error, response, body) {
     if (error) return callback(error);
   	var lines = body.split("\n");
-  	that.log("Sonoff HTTP: " + that.hostname + " Set State to: " + lines[1]);
+  	that.log("Sonoff HTTP: " + that.hostname + ", Relay " + that.relay + ", Set State to: " + lines[1]);
   	if (lines[1] == "POWER = OFF") callback()
   	else if (lines[1] == "POWER = ON") callback()
   })
