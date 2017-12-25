5b054b8be4ed0e853cc4716ce7bb9c7c3d4ac651
Alexy Mikhailichenko <alexymik@users.noreply.github.com>
2017-12-25 02:19:24 -0500

    Fixes #1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var Service, Characteristic;
 var request = require('request');
 
+function isSonoffOn(http_reply) {
+  
+}
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-
+  
   homebridge.registerAccessory("homebridge-sonoff-tasmota-http", "SonoffTasmotaHTTP", SonoffTasmotaHTTPAccessory);
 }
 
@@ -14,14 +17,14 @@ function SonoffTasmotaHTTPAccessory(log, config) {
   this.config = config;
   this.name = config["name"]
   this.hostname = config["hostname"] || "sonoff";
-
+  
   this.service = new Service.Outlet(this.name);
-
+  
   this.service
-    .getCharacteristic(Characteristic.On)
-    .on('get', this.getState.bind(this))
-    .on('set', this.setState.bind(this));
-    
+  .getCharacteristic(Characteristic.On)
+  .on('get', this.getState.bind(this))
+  .on('set', this.setState.bind(this));
+  
   this.log("Sonoff Tasmota HTTP Initialized")
 }
 
@@ -29,10 +32,16 @@ SonoffTasmotaHTTPAccessory.prototype.getState = function(callback) {
   var that = this
   request("http://" + that.hostname + "/cm?cmnd=Power", function(error, response, body) {
     if (error) return callback(error);
-  	var lines = body.split("\n");
-  	that.log("Sonoff HTTP: " + that.hostname + " Get State: " + lines[1]);
-  	if (lines[1] == "POWER = OFF") callback(null, 0)
-  	else if (lines[1] == "POWER = ON") callback(null, 1)
+    var sonoff_reply = JSON.parse(body); // {"POWER":"ON"}
+    that.log("Sonoff HTTP: " + that.hostname + " Get State: " + JSON.stringify(sonoff_reply));
+    switch (sonoff_reply["POWER"]) {
+      case "ON":
+        callback(null, 1);
+        break;
+      case "OFF":
+        callback(null, 0);
+        break;
+    }
   })
 }
 
@@ -42,10 +51,16 @@ SonoffTasmotaHTTPAccessory.prototype.setState = function(toggle, callback) {
   var that = this
   request("http://" + that.hostname + "/cm?cmnd=Power" + newstate, function(error, response, body) {
     if (error) return callback(error);
-  	var lines = body.split("\n");
-  	that.log("Sonoff HTTP: " + that.hostname + " Set State to: " + lines[1]);
-  	if (lines[1] == "POWER = OFF") callback()
-  	else if (lines[1] == "POWER = ON") callback()
+    var sonoff_reply = JSON.parse(body); // {"POWER":"ON"}
+    that.log("Sonoff HTTP: " + that.hostname + " State: " + JSON.stringify(sonoff_reply));
+    switch (sonoff_reply["POWER"]) {
+      case "ON":
+        callback();
+        break;
+      case "OFF":
+        callback();
+        break;
+    }
   })
 }
 
